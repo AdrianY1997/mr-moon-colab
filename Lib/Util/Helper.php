c3e2d7783f4e9021ab9a1591755d3ff5365986aa
HappyFox.Devs <yasnoadriandatos@gmail.com>
2023-03-19 14:20:48 -0500

    Change base biew

--- Lib/Util/Helper.php
+++ Lib/Util/Helper.php
@@ -24,7 +24,7 @@ if (!function_exists("render")) {
             ${$e} = $data[$e];
         }
 
-        include "App/Site/Views/app.base.php";
+        include "App/Site/Views/foxy.base.php";
     }
 }
 
