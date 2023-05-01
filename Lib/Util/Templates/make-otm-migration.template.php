7ffda2fad4984c66fe107afdd577237c1a87be07
AdrianY1997 <yasnoadriandatos@gmail.com>
2023-05-01 13:34:35 -0500

    Refac templates

--- Lib/Util/Templates/make-otm-migration.template.php
+++ Lib/Util/Templates/make-otm-migration.template.php
@@ -1,19 +1,18 @@
 <?php
 
-use FoxyMVC\Lib\Foxy\Database\Schema\Blueprint;
 use FoxyMVC\Lib\Foxy\Facades\Schema;
+use FoxyMVC\Lib\Foxy\Database\Schema\Blueprint;
 
-return new class
-{
-    public function up()
-    {
-        Schema::create("__tableName", function (Blueprint $table) {
+return new class {
+    private string $tableName = "__tableName";
+
+    public function up() {
+        Schema::create($this->tableName, function (Blueprint $table) {
             $table->id();
         });
     }
 
-    public function down()
-    {
-        Schema::dropIfExists("__tableName");
+    public function down() {
+        Schema::dropIfExists($this->tableName);
     }
 };
