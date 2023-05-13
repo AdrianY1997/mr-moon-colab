a4b88bfc0fe6d719b1b25fb9dbc7f3d29ec284e8
AdrianY1997 <yasnoadriandatos@gmail.com>
2023-05-13 13:07:20 -0500

    Refactor BaseModel to Table and try to implement chained function

--- Lib/Util/Templates/make-model.template.php
+++ Lib/Util/Templates/make-model.template.php
@@ -2,9 +2,9 @@
 
 namespace FoxyMVC\App\Models;
 
-use FoxyMVC\Lib\Foxy\Core\Base\DB;
+use FoxyMVC\Lib\Foxy\Database\Table;
 
-class __model extends DB {
+class __model extends Table {
     public function __construct() {
         parent::__construct("__tableName");
     }
