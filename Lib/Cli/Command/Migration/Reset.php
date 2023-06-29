0bb825e31f3a3be32fc438040111b61c6d6f4770
Adrian Mauricio Arandia Urrea <yasnoadriandatos@gmail.com>
2023-06-29 06:43:15 -0500

    Update Reset.php

--- Lib/Cli/Command/Migration/Reset.php
+++ Lib/Cli/Command/Migration/Reset.php
@@ -9,24 +9,26 @@ use FoxyMVC\Lib\Cli\Core\Base\Command;
 /**
  * Clase para migrar la base de datos
  */
-class Migrate extends Command {
-    /**
-     * Constructor de la clase Backup
-     *
-     * @param array $pro Propiedades
-     * @param array $avs Argumentos
-     */
-    public function __construct($pro, $avs) {
-        parent::__construct($pro, $avs);
-    }
+class Reset extends Command {
+  /**
+   * Constructor de la clase Backup
+   *
+   * @param array $pro Propiedades
+   * @param array $avs Argumentos
+   */
+  public function __construct($pro, $avs) {
+    parent::__construct($pro, $avs);
+  }
 
-    /**
-     * Inicializa la migración
-     *
-     * @return void
-     */
-    public function init() {
-      (new Rollback())->init();
-      (new Migrate())->init();
-    }
+  /**
+   * Inicializa la migración
+   *
+   * @return void
+   */
+  public function init() {
+    $rollback = new Rollback($this->property, $this->options);
+    $rollback->init();
+    $migrate = new Migrate($this->property, $this->options);
+    $migrate->init();
+  }
 }
