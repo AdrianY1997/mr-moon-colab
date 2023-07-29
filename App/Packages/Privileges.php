fb27e5e52a1132b8e669b268c2b28ed031449741
adriany1997 <yasnoadriandatos@gmail.com>
2023-07-29 11:25:07 -0500

    feat(user): Añadido usuario maestro

--- App/Packages/Privileges.php
+++ App/Packages/Privileges.php
@@ -5,11 +5,13 @@ namespace FoxyMVC\App\Packages;
 enum Privileges: int {
     case User = 1;
     case Admin = 2;
+    case Master = 4;
 
     public function get(): int {
         return match ($this) {
             static::User => 1,
             static::Admin => 2,
+            static::Master => 4
         };
     }
 }
