c1eff5f5797124171bdd95e75bf76f8b1a6352e8
adriany1997 <yasnoadriandatos@gmail.com>
2023-08-02 11:21:06 -0500

    chore(events): Actualización vista

--- Lib/Sly/Patterns/ClassPatterns.php
+++ Lib/Sly/Patterns/ClassPatterns.php
@@ -10,7 +10,10 @@ class ClassPatterns implements TemplatePatterns {
     public function getPatterns() {
         return [
             '/([A-Z][a-z]+)::/' => function ($matches) {
-                $file = search_file(constant("DIR"), $matches[1] . ".php");
+                $file = search_file(constant("DIR") . "/App", $matches[1] . ".php");
+                if (count($file) == 0) {
+                    $file = search_file(constant("DIR") . "/Lib/Foxy", $matches[1] . ".php");
+                }
                 $class = "FoxyMVC" . explode(".php", str_replace("/", '\\', explode(constant("DIR"), $file[0])[1]))[0];
                 return $class . "::";
             }
