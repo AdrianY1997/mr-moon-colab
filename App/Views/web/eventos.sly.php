38fbf3360dbebb03d1553c47c721506dbfcd9dc5
adriany1997 <yasnoadriandatos@gmail.com>
2023-08-02 11:46:40 -0500

    chore(events): Actualización

--- App/Views/web/eventos.sly.php
+++ App/Views/web/eventos.sly.php
@@ -2,14 +2,14 @@
     <div class="title" style="background-image: url({{ asset('img/eventos/event-title-img.png') }})">
         <h1>Eventos</h1>
     </div>
-    <div class="imgs container">
+    <div class="imgs container position-relative p-0">
         @if(!$events):
         <tr>
             <td>No hay Eventos.</td>
         </tr>
         @else
         @foreach($events as $evento):
-        <div class="evento-item" data-event-href="{{ route("event.get", ["id" => 1]) }}">
+        <div class="evento-item" data-event-href="{{ route("event.get", ["id" => $evento->even_id]) }}" type="button" data-bs-toggle="modal" data-bs-target="#show-event">
             <div class="imagen">
                 <img class="w-100" src="{{ asset($evento->even_path) }}" alt="">
             </div>
@@ -22,6 +22,16 @@
         @endforeach
         @endif
     </div>
-  </div>
-  <script src="{{ asset("js/eventos/show.js") }}"></script>
-  <script src="Public/js/script.js"></script>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="show-event" tabindex="-1" aria-labelledby="show-eventLabel" aria-hidden="true" style="width: 100%">
+    <div class="modal-dialog w-100" style="max-width: 100%;">
+        <div class="modal-content container p-3">
+            Hola
+        </div>
+    </div>
+</div>
+
+<script src="{{ asset("js/eventos/show.js") }}"></script>
+<script src="Public/js/script.js"></script>
