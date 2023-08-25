af6e5db96ece7269cda2288a665403c4baf2ed48
juandavid2587 <juansanchez258741@gmail.com>
2023-08-25 10:04:02 -0500

    cambios eventos3

--- App/Views/dashboard/galeria.sly.php
+++ App/Views/dashboard/galeria.sly.php
@@ -3,20 +3,20 @@
         <div class="container">
             @include('dashboard/static/menu'):
             <div class="content d-flex gap-3">
-                <div class="dash-gallery-item-container" data-img="main">
+                <div class="dash-gallery-item-container">
                     <p></p>
-                    <img class="gal-img" src="{{ asset("img/gallery/home-event.jpg") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/home-drink.jpg") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/cafebar}.jpg") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/home-food.jpg") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/unnamed (1).png") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/granizado-de-cafe-2.jpg") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/unnamed (2).png") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/unnamed.png") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/unnamed (3).png") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/copteles.jpeg") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/cockteleles-modernos.jpg") }}" alt="">
-                    <img class="gal-img" src="{{ asset("img/gallery/50caf20e7f61dbe6fd88d1d18af34420.jpg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/home-event.jpg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/home-drink.jpg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/cafebar}.jpg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/home-food.jpg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/unnamed (1).png") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/granizado-de-cafe-2.jpg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/unnamed (2).png") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/unnamed.png") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/unnamed (3).png") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/copteles.jpeg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/cockteleles-modernos.jpg") }}" alt="">
+                    <img class="gali-img" src="{{ asset("img/gallery/50caf20e7f61dbe6fd88d1d18af34420.jpg") }}" alt="">
                 </div>
             </div>
         </div>
