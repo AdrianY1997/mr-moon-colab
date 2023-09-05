f127a7c487c64b809ff4509194ee3149a29c98e4
EstebanCrdoba <ecordoba08@misena.edu.co>
2023-09-05 08:53:05 -0500

    Validacion

--- App/Views/dashboard/eventos.sly.php
+++ App/Views/dashboard/eventos.sly.php
@@ -70,7 +70,7 @@
                             </div>
                             <div class="form-label-group mb-3">
                                 <label for="item-path">Imagen</label>
-                                <input required type="file" id="item-path" name="item-path">
+                                <input name="image" required type="file" id="item-path" name="item-path">
                             </div>
                             <div class="d-flex justify-content-between">
                                 <button type="submit" class="btn btn-primary">Agregar</button>
