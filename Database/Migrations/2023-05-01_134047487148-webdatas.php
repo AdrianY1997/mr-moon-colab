0314e6f342acbccc3664cb6d8218c6cf9e8860de
EstebanCrdoba <ecordoba08@misena.edu.co>
2023-09-06 08:35:04 -0500

    m&v

--- Database/Migrations/2023-05-01_134047487148-webdatas.php
+++ Database/Migrations/2023-05-01_134047487148-webdatas.php
@@ -20,8 +20,8 @@ return new class {
             $table->string("twlink");
             $table->string("iglink");
             $table->string("ytlink");
-            $table->string("mision");
-            $table->string("vision");
+            $table->string("m");
+            $table->string("v");
         });
 
         Schema::insert($this->tableName, [
@@ -36,8 +36,8 @@ return new class {
             "twlink" => "https://twitter.com/",
             "iglink" => "https://instagram.com/",
             "ytlink" => "https://www.youtube.com/",
-            "mision"=>"En nuestra empresa aspira ser un negocio que sastistace las necesidades de nuestros clientes",
-            "vision"=>"En el año 2035 ser uno de los mas negocios mas populares y tener diferentes surcusales en el pais"
+            "m"=>"En nuestra empresa aspira ser un negocio que sastistace las necesidades de nuestros clientes",
+            "v"=>"En el año 2035 ser uno de los mas negocios mas populares y tener diferentes surcusales en el pais"
         ]);
     }
 
